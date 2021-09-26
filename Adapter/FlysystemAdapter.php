6bf62d545476e0e93a74f4c28434ed7c4dc93fa0
Joshua Parker <josh@joshuaparker.blog>
2021-09-26 00:05:43 -0700

    Updated created file version.

--- Adapter/FlysystemAdapter.php
+++ Adapter/FlysystemAdapter.php
@@ -7,7 +7,7 @@
  * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
  * @license    https://opensource.org/licenses/mit-license.php MIT License
  *
- * @since      1.0.0
+ * @since      1.1.0
  */
 
 declare(strict_types=1);
