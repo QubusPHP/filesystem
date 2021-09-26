955e47347862c13a155b0f09e2f3091be43a86d6
Joshua Parker <josh@joshuaparker.blog>
2021-09-26 00:01:30 -0700

    More robust Filesystem using League Flysystem.

---
+++ Adapter/AwsS3FlysystemAdapter.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Qubus\FileSystem
+ *
+ * @link       https://github.com/QubusPHP/filesystem
+ * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
+ * @license    https://opensource.org/licenses/mit-license.php MIT License
+ *
+ * @since      1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Qubus\FileSystem\Adapter;
+
+use Aws\S3\S3ClientInterface;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter as LeagueAwsS3V3Adapter;
+use League\Flysystem\AwsS3V3\PortableVisibilityConverter;
+use League\Flysystem\Visibility;
+use Qubus\Config\ConfigContainer;
+
+final class AwsS3FlysystemAdapter extends LeagueAwsS3V3Adapter implements FlysystemAdapter
+{
+    private ConfigContainer $config;
+
+    public function __construct(S3ClientInterface $client, ConfigContainer $config)
+    {
+        $this->config = $config;
+
+        parent::__construct(
+            $client,
+            $this->config->getConfigKey('filesystem.awsS3.bucket'),
+            $this->config->getConfigKey('filesystem.awsS3.prefix'),
+            new PortableVisibilityConverter(
+                $this->config->getConfigKey('filesystem.awsS3.visibility', Visibility::PUBLIC)
+            )
+        );
+    }
+}
---
+++ Adapter/LocalFlysystemAdapter.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Qubus\FileSystem
+ *
+ * @link       https://github.com/QubusPHP/filesystem
+ * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
+ * @license    https://opensource.org/licenses/mit-license.php MIT License
+ *
+ * @since      1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Qubus\FileSystem\Adapter;
+
+use League\Flysystem\Local\LocalFilesystemAdapter as LeagueLocalFileSystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use League\MimeTypeDetection\MimeTypeDetector;
+use Qubus\Config\ConfigContainer;
+
+use const LOCK_EX;
+
+final class LocalFlysystemAdapter extends LeagueLocalFileSystemAdapter implements FlysystemAdapter
+{
+    private ConfigContainer $config;
+
+    public function __construct(
+        ConfigContainer $config,
+        int $writeFlags = LOCK_EX,
+        int $linkHandling = self::DISALLOW_LINKS,
+        ?MimeTypeDetector $mimeTypeDetector = null
+    ) {
+        $this->config = $config;
+
+        parent::__construct(
+            $this->config->getConfigKey('filesystem.local.root'),
+            PortableVisibilityConverter::fromArray($this->setVisibilityConverter()),
+            $writeFlags,
+            $linkHandling,
+            $mimeTypeDetector
+        );
+    }
+
+    /**
+     * The directory and file visibility options.
+     *
+     * @return array
+     */
+    private function setVisibilityConverter(): array
+    {
+        return [
+            'file' => [
+                'public'  => $this->config->getConfigKey('filesystem.local.visibility.file.public', 0644),
+                'private' => $this->config->getConfigKey('filesystem.local.visibility.file.private', 0600),
+            ],
+            'dir'  => [
+                'public'  => $this->config->getConfigKey('filesystem.local.visibility.dir.public', 0755),
+                'private' => $this->config->getConfigKey('filesystem.local.visibility.dir.private', 0700),
+            ],
+        ];
+    }
+}
